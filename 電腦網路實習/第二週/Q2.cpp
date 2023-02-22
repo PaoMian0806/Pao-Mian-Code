e4c14d0e82ba9c5bcf808f27638be8f8206b5fca
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-02-22 15:09:32 +0800

    Create Q2.cpp

---
+++ 電腦網路實習/第二週/Q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    for (int i = 1; i <= 10; i++){
+        x += i;
+    }
+    cout<<x;
+    return 0;
+}
